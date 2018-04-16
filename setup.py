15027ff81ef1bca461b3d04cce348e68e47eaa06
erikvw <ew2789@gmail.com>
2018-04-16 18:26:55 +0200

    travis

--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='ambition-validators',
     version='0.1.1',
-    author=u'lkepaletswe',
+    author=u'lkepaletswe,erikvw@gmail.com',
     author_email='',
     packages=find_packages(),
     include_package_data=True,
-    url='https://github.com/botswana-harvard/ambition-validators',
+    url='https://github.com/ambition-study/ambition-validators',
     license='GPL license, see LICENSE',
     description='ambition',
     long_description=README,
