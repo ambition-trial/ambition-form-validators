e8c2ee29e4378b3c75d5de57e0513716b3b3b68e
erikvw <ew2789@gmail.com>
2019-11-22 14:02:25 -0600

    test setup

--- runtests.py
+++ runtests.py
@@ -23,8 +23,9 @@ DEFAULT_SETTINGS = DefaultTestSettings(
     SUBJECT_REQUISITION_MODEL="ambition_subject.subjectrequisition",
     ADVERSE_EVENT_ADMIN_SITE="ambition_ae_admin",
     ADVERSE_EVENT_APP_LABEL="ambition_ae",
-    RANDOMIZATION_LIST_PATH=join(
-        base_dir, app_name, "tests", "test_randomization_list.csv"),
+    EDC_RANDOMIZATION_LIST_MODEL="ambition_rando.randomizationlist",
+    EDC_RANDOMIZATION_LIST_FILE=join(
+        base_dir, app_name, "tests", "etc", "randomization_list.csv"),
     INSTALLED_APPS=[
         "django.contrib.admin",
         "django.contrib.auth",
